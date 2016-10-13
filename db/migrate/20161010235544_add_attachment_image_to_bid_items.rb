794adb0b1e628f5b9f81ec276d23890472d49599
BingruiDong1992 <jerry.dbr@gmail.com>
2016-10-12 17:45:11 -0700

    User login, signup, logout, relate item with user_id

--- db/migrate/20161010235544_add_attachment_image_to_bid_items.rb
+++ db/migrate/20161010235544_add_attachment_image_to_bid_items.rb
@@ -1,8 +1,6 @@
 class AddAttachmentImageToBidItems < ActiveRecord::Migration
   def self.up
-    change_table :bid_items do |t|
-      t.attachment :image
-    end
+    add_attachment :bid_items  :image
   end
 
   def self.down
