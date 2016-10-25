63be7fa3e7c2521795fcd696da6d423fc92d66e5
chienyu_chan <b99611040@ntu.edu.tw>
2016-10-24 21:21:00 -0700

    new a Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   devise_for :users
   resources :bid_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
