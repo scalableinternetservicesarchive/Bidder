b1e7f9fb110bc4649c4edf7491f1025e6a2a3d89
chienyu_chan <b99611040@ntu.edu.tw>
2016-11-02 15:19:20 -0700

    Add S3 config to production.rb Add AWS version 1.6 in Gemfile Add S3 storage in model

--- app/models/bid_item.rb
+++ app/models/bid_item.rb
@@ -2,7 +2,7 @@ class BidItem < ApplicationRecord
   #searchkick
 	validates :item_name, :presence => true
 
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, storage: :s3
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 	belongs_to :seller, class_name: "User", foreign_key: "seller_id"
