1d0cd70f582262ae89df70cecef64efad8c54ac6
chienyu_chan <b99611040@ntu.edu.tw>
2016-11-04 01:39:35 -0700

    take out application.rb code

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,4 @@ module Bidder
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-  config.paperclip_defaults = {
-    storage: :s3,
-    bucket: ENV['S3_BUCKET_NAME'],
-    s3_credentials: {
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
 end
