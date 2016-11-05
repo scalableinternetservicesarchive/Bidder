f53f435eae036f140095cc6feefffd5244670dde
chienyu_chan <b99611040@ntu.edu.tw>
2016-11-05 01:21:28 -0700

    1) Update the database, now we have community at the beginning. 2) Update the community show page, now there are two tables side by side 3) Sold items can be shown in community show page 4) Indent on users profile page

--- generate.py
+++ generate.py
@@ -1,11 +1,21 @@
+import random
+
 text_file = open("seeds.rb", "w")
-for i in range(1,50):
+comm_count = 0
+for i in range(1,51):
     text_file.write(
-        "User.create(email: '" + str(i) + "@" + str(i) + "', password: '123456', password_confirmation: '123456')\n")
+        "User.create(email: '" + str(i) + "@ucsb', password: '123456', password_confirmation: '123456')\n")
 
+    if i%10 == 1:
+    	comm_count += 1
+    	text_file.write(
+    		"Community.create(name: 'Com" + str(comm_count) + "')\n")
 
-    for j in range(1,10):
+    text_file.write(
+    	"UserCommunity.create(community_id: " + str(comm_count) + ", user_id: " + str(i) + ")\n")
+
+    for j in range(1,11):
         text_file.write(
-            "BidItem.create(item_name: '" + "Item" + str(j) + "belong_to" + str(i) + "', starting_price: 10, current_price: 10, fixed_price: 1000, seller_id: "+ str(i) + ")\n")
+            "BidItem.create(item_name: 'Item" + str(j) + "belong_to" + str(i) + "', starting_price: 10, current_price: 10, fixed_price: " + str(random.randint(800,10000)) + ", seller_id: "+ str(i) + ")\n")
 
 text_file.close()
