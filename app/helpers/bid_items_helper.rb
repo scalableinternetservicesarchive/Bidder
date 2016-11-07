7e89e533e99d927ec9187799b7efda17fc0d988e
Yiting-Mao <rachelym93@gmail.com>
2016-11-06 17:24:22 -0800

    add server side cache for item index and show page

--- app/helpers/bid_items_helper.rb
+++ app/helpers/bid_items_helper.rb
@@ -1,2 +1,9 @@
 module BidItemsHelper
+  def cache_key_for_biditem_table
+    "bid_item-table-#{BidItem.maximum(:updated_at)}"
+  end
+
+  def cache_key_for_biditem_row(bid_item)
+    "bid_item-#{bid_item.id}-#{bid_item.updated_at}"
+  end
 end
