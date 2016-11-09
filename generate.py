52714b5ae52a0bf6e5fc4be7a67f66af9b6b3ee9
chienyu_chan <b99611040@ntu.edu.tw>
2016-11-08 21:50:25 -0800

    update database

--- generate.py
+++ generate.py
@@ -6,20 +6,11 @@ for i in range(1,51):
     text_file.write(
         "User.create(email: '" + str(i) + "@ucsb', password: '123456', password_confirmation: '123456')\n")
 
-    if i%10 == 1:
-    	comm_count += 1
-    	text_file.write(
-    		"Community.create(name: 'Com" + str(comm_count) + "')\n")
-
-    text_file.write(
-    	"UserCommunity.create(community_id: " + str(comm_count) + ", user_id: " + str(i) + ")\n")
-
     for j in range(1,11):
         time = str(random.randint(1,100))
         text_file.write(
             "BidItem.create(item_name: 'Item" + str(j) + "belong_to" + str(i) + 
             "', starting_price: 10, current_price: 10, fixed_price: " + 
-            str(random.randint(800,10000)) + ", bid_time: " + time + 
-            ", bid_time_left: " + time + ", seller_id: "+ str(i) + ")\n")
+            str(random.randint(800,10000)) + ", seller_id: "+ str(i) + ")\n")
 
 text_file.close()
