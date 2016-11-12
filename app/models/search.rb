0042e855b8ad352cd38ea6b5a17e088aa54b416d
chienyu_chan <b99611040@ntu.edu.tw>
2016-11-11 17:39:30 -0800

    Now we can search by community and item name together. But there is a bug

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,25 +6,28 @@ class Search < ApplicationRecord
   private
 
   def find_products
-    products = BidItem.where("item_name like ?", "%#{keywords}%") if keywords.present?
-
-    # products = []
-    
-    # if category_id.present?
-    #   community = Community.where(id: category_id) 
-    #   users=[]
-    #   community.first.user_communities.each do |uc|
-    #     users << User.find(uc.user_id)
-    #   end
-
-    #   users.each do |user|
-    #     item = BidItem.where(:seller_id => user.id)
-    #     item.each do |item|
-    #       products << item
-    #     end
-    #   end
-    # end 
-
+    products = []
+    if category_id.present?
+      community = Community.find(category_id) 
+      community.user_communities.each do |uc|
+        items = BidItem.where(:seller_id => User.find(uc.user_id))
+        
+        if keywords.present?
+          items_filter_name = items.where("item_name like ?", "%#{keywords}%") 
+          if items_filter_name.any?
+            items_filter_name.each do |item|
+            products << item
+            end
+          end
+        else
+          items.each do |item|
+            products << item
+          end
+        end
+      end
+    else
+      products = BidItem.where("item_name like ?", "%#{keywords}%")
+    end
     products
   end
 end
