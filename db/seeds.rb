2af3299764e5ce2006c7cb70ed3232bd1b694c14
chienyu_chan <b99611040@ntu.edu.tw>
2016-11-24 12:22:59 -0800

    Update seed file and flows

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,27 @@
-n = 0
 r = Random.new
+o = [('a'..'z')].map { |i| i.to_a }.flatten
 
-(1..800).each do |i|
-	User.create(email: i.to_s+'_broswer@ucsb', password: '123456', password_confirmation: '123456')
+
+puts 'Generating 1000 users...'
+(1..1000).each do |i|
+	User.create(email: i.to_s+'@ucsb', password: '123456', password_confirmation: '123456')
 	n += 1
 end
+puts 'Generated 1000 users.'
 
-puts 'generated broswing users'
-
+puts 'Generating items for small sellers (id: 1 to 150)...'
 (1..150).each do |i|
-	User.create(email: i.to_s+'_small@ucsb', password: '123456', password_confirmation: '123456')
-	n += 1
-
 	total = r.rand(1..10)
 	total.times do |j|
-		BidItem.create(item_name: j.to_s+'item_of_small_'+i.to_s, starting_price: 10, current_price: 10, fixed_price: r.rand(800..10000), seller_id: n)
+		BidItem.create(item_name: (0...10).map { o[rand(o.length)] }.join, starting_price: 200, current_price: 200, fixed_price: r.rand(500..50000), seller_id: i)
 	end
 end
 
-(1..50).each do |i|
-	User.create(email: i.to_s+'_big@ucsb', password: '123456', password_confirmation: '123456')
-	n += 1
-
+puts 'Generating items for big sellers (id: 151 to 200)...'
+(151..200).each do |i|
 	total = r.rand(100..500)
 	total.times do |j|
-		BidItem.create(item_name: j.to_s+'item_of_big_'+i.to_s, starting_price: 10, current_price: 10, fixed_price: r.rand(800..10000), seller_id: n)
+		BidItem.create(item_name: (0...10).map { o[rand(o.length)] }.join, starting_price: 10, current_price: 10, fixed_price: r.rand(500..50000), seller_id: i)
 	end
 end
 
