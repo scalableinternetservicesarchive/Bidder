bcaf4f5d0187b3ea2527ba0fd6fa6c2f69f5c476
Yiting-Mao <rachelym93@gmail.com>
2016-11-24 23:21:42 -0800

    add search table cache, change seed and tsung bid price

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,2 +1,5 @@
 module SearchesHelper
+	def cache_key_for_search_show_table(search)
+		"search-show-table-#{search.keywords}-#{search.category_id}-#{search.min_price}-#{search.max_price}"
+	end
 end
