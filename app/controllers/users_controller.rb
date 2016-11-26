6ff481d4b85e9f9a398e3a601ebecaad2bf41d70
chienyu_chan <b99611040@ntu.edu.tw>
2016-11-25 18:49:52 -0800

    Limit profile to show only 10 selling items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
   def show
-  	# debugger
-  	# @user = User.find(params[:id])
-  	@bid_records = BidRecord.where(user_id: current_user).order("created_at DESC")  		
+  	@bid_records = BidRecord.where(user_id: current_user).order("created_at DESC")
   end
 end
