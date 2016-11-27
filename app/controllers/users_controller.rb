13dd81e8fe1c355cc9659f85f3ee36616c6aaf0e
Yiting-Mao <rachelym93@gmail.com>
2016-11-27 13:18:47 -0800

    add server cache

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
   def show
-  	@bid_records = BidRecord.where(user_id: current_user).order("created_at DESC").first(30)
-  	@sell_items = current_user.sell_items.order("created_at DESC").first(30)
-  	@buy_items = current_user.buy_items.order("created_at DESC").first(30)
   end
 end
--- app/helpers/communities_helper.rb
+++ app/helpers/communities_helper.rb
@@ -1,2 +1,15 @@
 module CommunitiesHelper
+	def cache_key_for_community_index_table
+		"community-index-table-#{Community.count}-#{Community.maximum(:id)}"
+	end
+
+  def cache_key_for_community_show_user_table(community)
+    "community-show-user-table-#{community.id}-#{community.user_communities.maximum(:id)}"
+  end
+
+  def cache_key_for_community_show_item_table(items) 
+    "community-show-item-table-#{items.maximum(:id)}"
+  end
+
+  
 end
