bfcda5f0507ef83382f25dfb5aecf228ae0d32cf
Yiting-Mao <rachelym93@gmail.com>
2016-11-26 18:25:11 -0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,72 @@
+
 r = Random.new
 o = [('a'..'z')].map { |i| i.to_a }.flatten
+n = 0
 
+users = []
+bid_items = []
+communities = []
+user_communities = []
 
-puts 'Generating 1000 users...'
-(1..1000).each do |i|
-	User.create(email: i.to_s+'@ucsb', password: '123456', password_confirmation: '123456')
-end
-puts 'Generated 1000 users.'
+#password = User.new(:password => "password").encrypted_password
 
-puts 'Generating items for small sellers (id: 1 to 150)...'
 (1..150).each do |i|
-	total = r.rand(1..10)
-	total.times do |j|
-		price = r.rand(200..5000)
-		BidItem.create(item_name: (0...10).map { o[rand(o.length)] }.join, starting_price: price, current_price: price, fixed_price: r.rand(15000..50000), seller_id: i)
+  users << "('#{i}@ucsb', '$2a$11$EqpBppDhqMxo7cdN34Wmh.A3GKM8/HhP64X8RikeNGWqry01czvJW','2000-01-01 12:34:56 UTC','2000-01-02 12:34:56 UTC')" 
+  n += 1
+  total = r.rand(1..10)
+  total.times do |j|
+    item_name = (0...10).map { o[rand(o.length)] }.join
+    price = r.rand(200..5000)
+    fix_price = r.rand(15000..50000)
+  	bid_items << "(#{n},'#{item_name}', #{price}, #{price}, #{fix_price},'2000-01-01 12:34:56 UTC','2000-01-02 12:34:56 UTC')" 	
 	end
-end
+end	
 
-puts 'Generating items for big sellers (id: 151 to 200)...'
+
 (151..200).each do |i|
-	total = r.rand(100..500)
-	total.times do |j|
-		price = r.rand(10..500)
-		BidItem.create(item_name: (0...10).map { o[rand(o.length)] }.join, starting_price: price, current_price: price, fixed_price: r.rand(15000..50000), seller_id: i)
-	end
+  users << "('#{i}@ucsb', '$2a$11$EqpBppDhqMxo7cdN34Wmh.A3GKM8/HhP64X8RikeNGWqry01czvJW','2000-01-01 12:34:56 UTC','2000-01-02 12:34:56 UTC')" 
+  n += 1
+  total = r.rand(100..500)
+  total.times do |j|
+    item_name = (0...10).map { o[rand(o.length)] }.join
+    price = r.rand(10..500)
+    fix_price = r.rand(15000..50000)
+    bid_items << "(#{n},'#{item_name}', #{price}, #{price}, #{fix_price},'2000-01-01 12:34:56 UTC','2000-01-02 12:34:56 UTC')"   
+  end
+end 
+
+(201..1000).each do |i|
+  users << "('#{i}@ucsb', '$2a$11$EqpBppDhqMxo7cdN34Wmh.A3GKM8/HhP64X8RikeNGWqry01czvJW','2000-01-01 12:34:56 UTC','2000-01-02 12:34:56 UTC')" 
+  n += 1 
 end
 
 (1..10).each do |i|
-	Community.create(name: 'Community_'+i.to_s)
+  communities << "('Community#{i}','2000-01-01 12:34:56 UTC','2000-01-02 12:34:56 UTC')"  
 end
 
 (1..1000).each do |i|
-	UserCommunity.create(community_id: r.rand(1..10), user_id: i) 
+  randnum=r.rand(1..10)
+  user_communities << "('#{randnum}',#{i},'2000-01-01 12:34:56 UTC','2000-01-02 12:34:56 UTC')"  
 end
+
+User.transaction do
+  User.connection.execute "INSERT INTO users (email, encrypted_password,created_at, updated_at) VALUES #{users.join(', ')}"
+  puts "added #{users.count} users "
+end
+
+BidItem.transaction do
+  BidItem.connection.execute "INSERT INTO bid_items (seller_id, item_name, starting_price,current_price,fixed_price, created_at, updated_at) VALUES #{bid_items.join(', ')}"
+  puts "added #{bid_items.count} bid_items "
+end
+
+Community.transaction do
+  Community.connection.execute "INSERT INTO communities (name,created_at, updated_at) VALUES #{communities.join(', ')}"
+  puts "added #{communities.count} communities "
+end
+UserCommunity.transaction do
+  UserCommunity.connection.execute "INSERT INTO user_communities (community_id,user_id,created_at, updated_at) VALUES #{user_communities.join(', ')}"
+  puts "added #{user_communities.count} user_communities "
+end
+
+
+
