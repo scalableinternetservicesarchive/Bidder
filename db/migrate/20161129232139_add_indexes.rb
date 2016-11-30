70625e079e7740cf92409eefbf777e5719cb5e9c
Yiting-Mao <rachelym93@gmail.com>
2016-11-29 16:56:39 -0800

    add index community_id for user_communities

--- db/migrate/20161129232139_add_indexes.rb
+++ db/migrate/20161129232139_add_indexes.rb
@@ -4,5 +4,6 @@ class AddIndexes < ActiveRecord::Migration[5.0]
   	add_index :bid_items, :seller_id
   	add_index :bid_items, :buyer_id
   	add_index :bid_items, :current_price
+  	add_index :user_communities, :community_id
   end
 end
